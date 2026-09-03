0179b62c0b1e2e0775f9c0aaad396133f60c1eb7
cmedranve <christian.medrano.ve@gmail.com>
2026-09-03 18:20:43 -0500

    Implementación de inicio automático luego de reinicio con system alert window

--- app/src/main/java/pe/com/scotiabank/blpm/android/knull/MainActivity.kt
+++ app/src/main/java/pe/com/scotiabank/blpm/android/knull/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -45,6 +46,14 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:${packageName}")
+            )
+            startActivity(intent)
+        }
 
         setContent {
             MaterialTheme {
